6b7aa748fa31f0d26aac2b101736515a14dfc9fa
Amirreza Shirazi <ashir025@sledge.cs.ucr.edu>
2021-07-20 17:21:12 -0700

    test

--- test.c
+++ test.c
@@ -3,20 +3,19 @@
 
 int main(int argc, char *argv[])
 {
-    printf(1, "This program is a Test for waitpid() \n");
+    printf(1, "This program is a Test for the waitpid() \n");
     int status, pid, i;
     for (i = 0; i < 5; i++)
     {
         pid = fork();
         if (!pid)
-        { //child
+        {
             sleep(8);
             printf(1, "Child PID = %d exited with: status = %d\n", getpid(), getpid() + 3);
             exit(getpid() + 3);
         }
         else
         {
-            //parent
             sleep(5);
             printf(1, "Parent waiting for Child PID = %d \n", pid);
             waitpid(pid, &status, 0);
